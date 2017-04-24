fe4a36be721ab587553c38119eec26d680c78640
maxmurder <rusty657@shaw.ca>
2017-04-23 19:39:56 -0700

    add the draft in publishPage script

--- publishPage.sh
+++ publishPage.sh
@@ -2,6 +2,6 @@
 
 FILE="$(date +%Y-%m-%d)-$(basename $1)"
 cp $1 _posts/$FILE
-git add _posts/$FILE
-git commit _posts/$FILE -m "publish $FILE"
+git add $1 _posts/$FILE
+git commit $1 _posts/$FILE -m "publish $FILE"
 git push
